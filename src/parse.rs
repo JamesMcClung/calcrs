9e02841e6370933dd81972f0dadbec69284efa72
James McClung <james@themcclungs.net>
2023-09-22 23:25:09 -0400

    parse (test): +parse_int

--- src/parse.rs
+++ src/parse.rs
@@ -5,3 +5,17 @@ pub use expr::{Expression, Value};
 pub fn parse(expr: &str) -> Result<Expression, String> {
     return Err(String::from(expr));
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_int() -> Result<(), String> {
+        assert!(matches!(parse("0")?.eval()?, Value::Integer(0)));
+        assert!(matches!(parse("11111")?.eval()?, Value::Integer(11111)));
+        assert!(matches!(parse("-32")?.eval()?, Value::Integer(-32)));
+        assert!(matches!(parse("+234")?.eval()?, Value::Integer(234)));
+        Ok(())
+    }
+}
