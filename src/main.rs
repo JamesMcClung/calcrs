df37d61520a74e48320b05c1180e29f3ff9cec75
James McClung <james@themcclungs.net>
2023-09-23 00:10:46 -0400

    main: eval input in loop

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,13 @@ pub mod parse;
 pub mod prompt;
 
 fn main() {
-    println!("Hello, world!");
+    for input in prompt::Prompt {
+        match parse::parse(&input) {
+            Ok(expr) => println!("{}", expr.eval().unwrap()),
+            Err(expr) if expr.is_empty() => return,
+            Err(expr) => {
+                println!("Couldn't parse: {expr}")
+            }
+        }
+    }
 }
