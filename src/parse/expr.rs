59fa83fc6f23b22a5859bbff68762bba2c11645c
James McClung <james@themcclungs.net>
2023-09-22 23:19:51 -0400

    expr: impl Display for Value

--- src/parse/expr.rs
+++ src/parse/expr.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug)]
 pub enum Expression {
     Constant(Value),
@@ -13,5 +15,14 @@ impl Expression {
         match self {
             Self::Constant(c) => Ok(c),
         }
+    }
+}
+
+impl Display for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Integer(num) => f.write_str(&format!("{num}"))?,
+        };
+        Ok(())
     }
 }
