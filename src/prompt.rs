83f07c8726cd7127b6daf25c5e2a2f06fa48618b
James McClung <james@themcclungs.net>
2023-09-23 00:10:13 -0400

    prompt: read from stdin

--- src/prompt.rs
+++ src/prompt.rs
@@ -1,9 +1,14 @@
+use std::io;
 pub struct Prompt;
 
 impl Iterator for Prompt {
     type Item = String;
 
     fn next(&mut self) -> Option<Self::Item> {
-        None
+        print!("> ");
+        io::Write::flush(&mut io::stdout()).expect("flush failed");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("read_line failed");
+        Some(input.trim().to_string())
     }
 }
