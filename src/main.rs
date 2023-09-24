f48af1a532f669b5c93586d2a5ceab3821b8ea11
James McClung <james@themcclungs.net>
2023-09-23 21:22:02 -0400

    main: fix exit-by-empty-line

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ pub mod prompt;
 
 fn main() {
     for input in prompt::Prompt {
+        if input.is_empty() {
+            return;
+        }
         match parse::parse(&input) {
             Ok(expr) => println!("{}", expr.eval().unwrap()),
-            Err(expr) if expr.is_empty() => return,
             Err(expr) => {
                 println!("Couldn't parse: {expr}")
             }
