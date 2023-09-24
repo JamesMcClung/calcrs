e88746529d5b553f508fec71753c031721afa68f
James McClung <james@themcclungs.net>
2023-09-24 09:51:57 -0400

    error: +TokenizeError

--- src/parse/error.rs
+++ src/parse/error.rs
@@ -2,6 +2,7 @@ use super::Token;
 
 #[derive(Debug)]
 pub enum Error {
+    TokenizeError(char),
     SyntaxError(Vec<Token>),
     EvalError(String),
 }
@@ -9,6 +10,7 @@ pub enum Error {
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(&match self {
+            Self::TokenizeError(c) => format!("TokenizeError: unknown character '{c}'"),
             Self::SyntaxError(tokens) => format!("SyntaxError: invalid syntax {tokens:?}"),
             Self::EvalError(s) => format!("EvalError: {s}"),
         })
