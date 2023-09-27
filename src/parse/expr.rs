7c9f2e3b5197b37cdb1980c386ba9c1fcd6be190
James McClung <james@themcclungs.net>
2023-09-26 21:39:28 -0400

    expr: impl Add for Value

--- src/parse/expr.rs
+++ src/parse/expr.rs
@@ -15,9 +15,7 @@ impl Expression {
     pub fn eval(self) -> Result<Value, Error> {
         match self {
             Self::Constant(c) => Ok(c),
-            Self::Sum(left, right) => match (left.eval()?, right.eval()?) {
-                (Value::Integer(left), Value::Integer(right)) => Ok(Value::Integer(left + right)),
-            },
+            Self::Sum(left, right) => Ok(left.eval()? + right.eval()?),
         }
     }
 }
@@ -27,5 +25,14 @@ impl std::fmt::Display for Value {
         f.write_str(&match self {
             Self::Integer(num) => format!("{num}"),
         })
+    }
+}
+
+impl std::ops::Add for Value {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (Value::Integer(lhs), Value::Integer(rhs)) => Value::Integer(lhs + rhs),
+        }
     }
 }
