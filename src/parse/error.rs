9325bdc08fadf081d4952d05acacf7d932e16809
James McClung <james@themcclungs.net>
2023-09-29 10:41:19 -0400

    error: rm prefix from SyntaxError message
    it won't always apply

--- src/parse/error.rs
+++ src/parse/error.rs
@@ -9,7 +9,7 @@ impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(&match self {
             Self::TokenizeError(c) => format!("TokenizeError: unknown character '{c}'"),
-            Self::SyntaxError(s) => format!("SyntaxError: invalid syntax \"{s}\""),
+            Self::SyntaxError(s) => format!("SyntaxError: {s}"),
             Self::EvalError(s) => format!("EvalError: {s}"),
         })
     }
